75cb51b3372cb1668f11cb49696ca332ab85c4b5
gustavcampos <gustavop.campos2004@gmail.com>
2024-05-21 10:27:38 -0300

    delete old files

--- ducksql/test_merged.py
+++
@@ -1,8 +0,0 @@
-import duckdb
-
-conn_merge = duckdb.connect("output_juntadb/databasemuitofoda.db")
-
-tables = conn_merge.sql("SHOW TABLES").fetchall()
-
-query = conn_merge.sql(f"Select * from {tables[1][0]}")
-print(query)
